cd7e72147d9184de520478a1afb931faa1603d01
diopmorfall <morfall.papydiop99@gmail.com>
2023-01-21 20:43:22 +0100

    Basic redux implementations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 export default function App() {
     return (
         <div className="App">
-        <h1>Redux Counter</h1>
+            <h1>Redux Counter</h1>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,52 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+//* an action kinda describes what you want to do (for example, increment or decrement); it returns the type of the action
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+const counterReducer = (state = 0, action) => { //* the reducer describes how the state gets transformed by the action
+    //? it needs the value of the state which we update, and the action
+
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state; //! if we don't have a covered case, we should return the current state
+    }
+}
+
+const store = configureStore({
+    //* we configure the store with a reducer object, that has all the reducers we need
+    //? they get combined because combineReducer() is called behind the scenes
+    reducer: {
+        counter: counterReducer
+    }
+}); //* the store is kinda a globalized state, that is accessible to any component
+store.subscribe(() => console.log(store.getState())) //? getState returns the current state
+
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decrement())
+//* dispatch is the one running the action (make sure to call the method that has)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
